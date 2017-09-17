96cf0ddb508ccc93531396dde5588d26389cf7c3
huangq <18093190651@163.com>
2017-09-17 15:07:10 +0800

    链表相加

--- src/com/hq/exercise_9_4/AddTwoNumbers.java
+++ src/com/hq/exercise_9_4/AddTwoNumbers.java
@@ -10,22 +10,35 @@ public class AddTwoNumbers {
 
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         boolean isCarry = false;
+        int val1 = 0, val2 = 0;
+        int newWal = 0;
+        ListNode headNode = new ListNode(0);
+        ListNode temNode = headNode;
         while (l1 != null || l2!= null) {
-            if (l1 == null) {
-                l1.val = l1.val + l2.val;
-                if (isCarry) {
-                    l1.val += 1;
-                    isCarry = false;
-                }
-                if (l1.val / 10 > 0) {
-                    isCarry = true;
-                    l1.val %= 10;
-                }
-            } else if (l2 == null) {
-
+            if (l1 != null) {
+                val1 = l1.val;
             }
+            if (l2 != null) {
+                val2 = l2.val;
+            }
+            int sum = 0;
+            if (isCarry) {
+                sum = val1 + val2 + 1;
+                isCarry = false;
+            } else {
+                sum = val1 + val2;
+            }
+            if (sum >= 10) {
+                newWal = sum%10;
+                isCarry = true;
+            }
+            ListNode newNode = new ListNode(newWal);
+            headNode.next = newNode;
+            headNode = headNode.next;
+            l1 = l1.next;
+            l2 = l2.next;
         }
-        return null;
+        return temNode.next;
     }
 
     class ListNode {
@@ -34,6 +47,8 @@ public class AddTwoNumbers {
         ListNode(int val){
             if (val > 0 && val < 10) {
                 this.val = val;
+            } else {
+                this.val = 0;
             }
         }
     }
